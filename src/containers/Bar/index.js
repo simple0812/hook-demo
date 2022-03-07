c86f4cfc75c3d4ad94cdb6a84c25481987bc5e9d
zhanglei <nonocase@sina.cn>
2022-03-07 14:06:43 +0800

    feat: 通过装饰器重写方法

--- src/containers/Bar/index.js
+++ src/containers/Bar/index.js
@@ -1,13 +1,21 @@
 // 使用装饰器复写方法
-const deprecated = (deprecationReason) => {
+const overrideFn = (fn) => {
   return (target, memberName, propertyDescriptor) => {
     return {
       get() {
+        let oldFn = propertyDescriptor.value;
+
+        if (!oldFn && propertyDescriptor.initializer) {
+          oldFn = propertyDescriptor.initializer();
+        }
+
         const wrapperFn = (...args) => {
-          console.log(
-            `Method ${memberName} is deprecated with reason: ${deprecationReason}`
-          );
-          return propertyDescriptor.value.apply(this, args);
+          if (Object.prototype.toString.call(fn) === '[object Function]') {
+            fn(...args);
+          }
+
+          let ret = oldFn.apply(this, args);
+          return ret;
         };
 
         Object.defineProperty(this, memberName, {
@@ -20,18 +28,38 @@ const deprecated = (deprecationReason) => {
     };
   };
 };
+
 class Foo {
   constructor(name) {
     this.name = name;
   }
 
-  @deprecated('adfasdfds')
+  get xx() {
+    return function (param) {
+      console.log('aaaaaaxx' + param);
+    };
+  }
+
+  @overrideFn(() => {
+    console.log('11111111111111');
+  })
   test() {
     console.log('test', this.name);
   }
+
+  @overrideFn(() => {
+    console.log('2222222222222');
+  })
+  testXX = () => {
+    console.log('testxx');
+  };
 }
 
 export default function () {
-  new Foo('bar').test();
+  let xFoo = new Foo('bar');
+
+  xFoo.testXX();
+  xFoo.test();
+  // xFoo.xx('1234');
   return <div>Bar</div>;
 }

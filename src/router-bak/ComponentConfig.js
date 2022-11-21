bfeaf4e51ff6a6e6c91476dbbdfcc334f4d996fc
zhanglei <Lei.Zhang@b-and-qchina.com>
2022-11-21 10:02:06 +0800

    feat:  清理代码

--- src/router-bak/ComponentConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-  foo: require('@/containers/Foo'),
-  bar: require('@/containers/Bar'),
-  exception403: require('@/containers/Exception/403'),
-  exception404: require('@/containers/Exception/404'),
-  exception500: require('@/containers/Exception/500')
-};
-
-export const BlankComponentsConfig = [];
